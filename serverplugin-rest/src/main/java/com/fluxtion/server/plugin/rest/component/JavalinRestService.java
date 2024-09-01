3cc594894513e9c2fddaa2e7a6cedb1124aa8c96
greg h <gregphiggins@gmail.com>
2024-09-01 11:53:57 +0100

    integrate Javalin as an adminCommandRequest processor

--- serverplugin-rest/src/main/java/com/fluxtion/server/plugin/rest/component/JavalinRestService.java
+++ serverplugin-rest/src/main/java/com/fluxtion/server/plugin/rest/component/JavalinRestService.java
@@ -5,11 +5,15 @@
 
 package com.fluxtion.server.plugin.rest.component;
 
+import com.fluxtion.runtime.annotations.Start;
+import com.fluxtion.runtime.annotations.runtime.ServiceRegistered;
 import com.fluxtion.runtime.lifecycle.Lifecycle;
 import com.fluxtion.server.dispatch.EventFlowManager;
 import com.fluxtion.server.dispatch.EventFlowService;
 import com.fluxtion.server.plugin.rest.service.CommandProcessor;
 import com.fluxtion.server.plugin.rest.service.RestController;
+import com.fluxtion.server.service.admin.AdminCommandRegistry;
+import com.fluxtion.server.service.admin.AdminCommandRequest;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -24,11 +28,18 @@ public class JavalinRestService implements EventFlowService, Lifecycle, RestCont
 
     private Javalin javalin;
     private EventFlowManager eventFlowManager;
+    private AdminCommandRegistry adminCommandRegistry;
 
     @Override
     public void setEventFlowManager(EventFlowManager eventFlowManager, String serviceName) {
-        log.info("Setting up Javalin REST service name:'{}'", serviceName);
+        log.info("set eventFlowManager name:'{}' for Javalin REST", serviceName);
         this.eventFlowManager = eventFlowManager;
+    }
+
+    @ServiceRegistered
+    public void adminRegistry(AdminCommandRegistry adminCommandRegistry, String name) {
+        log.info("Admin registry: '{}' name: '{}'", adminCommandRegistry, name);
+        this.adminCommandRegistry = adminCommandRegistry;
     }
 
     @Override
@@ -40,15 +51,30 @@ public class JavalinRestService implements EventFlowService, Lifecycle, RestCont
                     public void handle(@NotNull Context ctx) throws Exception {
                         StringBuilder sb = new StringBuilder();
                         eventFlowManager.appendQueueInformation(sb);
-                        ctx.json(new Message(sb.toString()));
+                        ctx.json(new Message("registered queues - " + sb.toString()));
+                    }
+                })
+                .post("/admin", ctx -> {
+                    AdminCommandRequest adminCommandRequest = ctx.bodyAsClass(AdminCommandRequest.class);
+                    adminCommandRequest.setOutput(out -> ctx.json(new Message(out.toString())));
+                    adminCommandRequest.setErrOutput(out -> ctx.json(new Message("Failure - " + out)));
+                    log.info("adminCommandRequest: {}", adminCommandRequest);
+                    if (adminCommandRegistry != null) {
+                        adminCommandRegistry.processAdminCommandRequest(adminCommandRequest);
                     }
                 })
                 .start(8080);
     }
 
+    @Start
+    public void start() {
+        log.info("starting Javalin REST service");
+    }
+
     @Override
     public void tearDown() {
         log.info("tear down Javalin REST service");
+        javalin.stop();
     }
 
     @Override
@@ -61,5 +87,11 @@ public class JavalinRestService implements EventFlowService, Lifecycle, RestCont
     @NoArgsConstructor
     public static class Message {
         private String message;
+    }
+
+    @Data
+    public static class AdminCommand {
+        private String command;
+        private String[] args = new String[0];
     }
 }
