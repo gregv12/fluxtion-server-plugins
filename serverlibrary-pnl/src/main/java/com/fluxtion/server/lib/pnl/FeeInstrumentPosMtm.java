98821f551f8a3004a76ad301ab161ade268edc9e
greg higgins <greg.higgins@v12technology.com>
2024-11-10 09:25:58 +0000

    Feature/pnl mtm (#41) (#42)
    * add mtm for as type
    
    * support mtm for instruments and global
    
    * simplify calculations using aggregate functions
    
    * add access to rates from calculator
    
    * fixes bad position calculations
    
    * fixes snapshot processing by moving to correct position
    
    * fixes snapshot processing by moving to correct position
    
    * setup is not running on DerivedTest on github ci, manual fix
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: greg h <gregphiggins@gmail.com>
    Co-authored-by: greg h <greg.higgins@nonco.com>

---
+++ serverlibrary-pnl/src/main/java/com/fluxtion/server/lib/pnl/FeeInstrumentPosMtm.java
@@ -0,0 +1,55 @@
+/*
+ * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package com.fluxtion.server.lib.pnl;
+
+import com.fluxtion.server.lib.pnl.refdata.Instrument;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class FeeInstrumentPosMtm {
+    private String bookName;
+    private Double fees = 0.0;
+    private Map<Instrument, Double> feesPositionMap = new HashMap<>();
+    private Map<Instrument, Double> feesMtmPositionMap = new HashMap<>();
+
+    public FeeInstrumentPosMtm reset() {
+        this.bookName = null;
+        this.fees = 0.0;
+        this.feesPositionMap.clear();
+        this.feesMtmPositionMap.clear();
+        return this;
+    }
+
+    public FeeInstrumentPosMtm resetMtm() {
+        getFeesMtmPositionMap().clear();
+        fees = 0.0;
+        return this;
+    }
+
+    public FeeInstrumentPosMtm combine(FeeInstrumentPosMtm from) {
+        if (from != null) {
+            this.fees += from.fees;
+
+            from.feesPositionMap.forEach((key, value) -> {
+                feesPositionMap.merge(key, value, Double::sum);
+            });
+
+            from.feesMtmPositionMap.forEach((key, value) -> {
+                feesMtmPositionMap.merge(key, value, Double::sum);
+            });
+
+            this.bookName = bookName == null ? from.bookName : bookName;
+        }
+        return this;
+    }
+
+    public double calcTradePnl() {
+        return fees = feesMtmPositionMap.values().stream().mapToDouble(Double::doubleValue).sum();
+    }
+}
---
+++ serverlibrary-pnl/src/main/java/com/fluxtion/server/lib/pnl/calculator/FeeInstrumentPosMtmAggregate.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package com.fluxtion.server.lib.pnl.calculator;
+
+import com.fluxtion.runtime.dataflow.aggregate.AggregateFlowFunction;
+import com.fluxtion.server.lib.pnl.FeeInstrumentPosMtm;
+import com.fluxtion.server.lib.pnl.Trade;
+
+public class FeeInstrumentPosMtmAggregate implements AggregateFlowFunction<Trade, FeeInstrumentPosMtm, FeeInstrumentPosMtmAggregate> {
+    private FeeInstrumentPosMtm feeInstrumentPosMtm = new FeeInstrumentPosMtm();
+
+    @Override
+    public FeeInstrumentPosMtm aggregate(Trade input) {
+        final String bookName = input.getFeeInstrument().instrumentName();
+        final double feePosition = input.getFee();
+        feeInstrumentPosMtm.setBookName(bookName);
+        feeInstrumentPosMtm.getFeesPositionMap().compute(input.getFeeInstrument(), (s, d) -> d == null ? feePosition : d + feePosition);
+        return feeInstrumentPosMtm;
+    }
+
+    @Override
+    public FeeInstrumentPosMtm get() {
+        return feeInstrumentPosMtm;
+    }
+
+    @Override
+    public FeeInstrumentPosMtm reset() {
+        feeInstrumentPosMtm = new FeeInstrumentPosMtm();
+        return feeInstrumentPosMtm;
+    }
+}
