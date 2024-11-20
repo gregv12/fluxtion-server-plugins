5b77223e2be12edd0b0c28fdd97c7efc7bb86464
greg h <gregphiggins@gmail.com>
2024-11-20 06:05:36 +0000

    upgrade to fluxtion 9.3.48, fluxtion-server 0.1.41

--- serverlibrary-pnl/src/main/java/com/fluxtion/server/lib/pnl/calculator/FluxtionPnlCalculator.java
+++ serverlibrary-pnl/src/main/java/com/fluxtion/server/lib/pnl/calculator/FluxtionPnlCalculator.java
@@ -81,8 +81,8 @@ import java.util.function.Consumer;
  *
  * <pre>
  * generation time                 : Not available
- * eventProcessorGenerator version : 9.3.47
- * api version                     : 9.3.47
+ * eventProcessorGenerator version : 9.3.48
+ * api version                     : 9.3.48
  * </pre>
  *
  * Event classes supported:
@@ -1427,6 +1427,7 @@ public class FluxtionPnlCalculator
   public void triggerCalculation() {
     throw new UnsupportedOperationException("triggerCalculation not supported");
   }
+  //EVENT BUFFERING - END
 
   private void auditEvent(Object typedEvent) {
     clock.eventReceived(typedEvent);
