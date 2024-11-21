a6fd093a32143ed3d7859789ba87f53e324c8f78
greg h <greg.higgins@nonco.com>
2024-11-21 08:20:04 +0000

    setup is not running on DerivedTest on github ci, manual fix

--- serverplugin-kafka/src/main/java/com/fluxtion/server/plugin/kafka/KafkaMessageConsumer.java
+++ serverplugin-kafka/src/main/java/com/fluxtion/server/plugin/kafka/KafkaMessageConsumer.java
@@ -6,51 +6,62 @@
 package com.fluxtion.server.plugin.kafka;
 
 import com.fluxtion.server.service.AbstractAgentHostedEventSourceService;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @Log4j2
 public class KafkaMessageConsumer extends AbstractAgentHostedEventSourceService {
 
     private KafkaConsumer<String, String> consumer;
+    @Getter
+    @Setter
     private Properties properties;
+    @Getter
+    @Setter
+    private String name;
 
     protected KafkaMessageConsumer(String name) {
         super(name);
     }
 
+    public KafkaMessageConsumer() {
+        super("kafka-consumer");
+    }
 
     @Override
     public void init() {
-        String groupId = "my-fourth-application";
-        String bootstrapServers = "127.0.0.1:9092";
 
-        // create consumer configs
-        properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        System.out.println("properties:" + properties);
+        log.info("properties:{}", properties);
+//        String groupId = "my-application";
+//        String bootstrapServers = "localhost:9092";
+
+//        // create consumer configs
+//        properties = new Properties();
+//        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+//        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         log.info("Initializing KafkaMessageConsumer {}", properties);
     }
 
     @Override
     public void start() {
         log.info("Starting KafkaMessageConsumer");
-        final String topic = "CONNECTOR.TOPIC_2";
+        final String topic = "CONNECTOR.TOPIC";
         // create consumer
         consumer = new KafkaConsumer<>(properties);
         // subscribe consumer to our topic(s)
-        consumer.subscribe(Arrays.asList(topic));
+        consumer.subscribe(List.of(topic));
     }
 
     @Override
@@ -68,5 +79,10 @@ public class KafkaMessageConsumer extends AbstractAgentHostedEventSourceService 
             log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
         }
         return records.count();
+    }
+
+    @Override
+    public String roleName() {
+        return name;
     }
 }
