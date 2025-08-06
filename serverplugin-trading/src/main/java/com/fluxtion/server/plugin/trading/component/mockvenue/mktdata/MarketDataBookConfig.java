cce6bafb095d28f631ed2c2ca766be6b3338d8b3
greg higgins <greg.higgins@v12technology.com>
2025-08-06 21:13:52 +0100

    configurable mock market data source (#84)
    Co-authored-by: greg higgins <greg.higgins@nonco.com>

---
+++ serverplugin-trading/src/main/java/com/fluxtion/server/plugin/trading/component/mockvenue/mktdata/MarketDataBookConfig.java
@@ -0,0 +1,20 @@
+package com.fluxtion.server.plugin.trading.component.mockvenue.mktdata;
+
+import lombok.Data;
+
+@Data
+public class MarketDataBookConfig {
+    private double minPrice;
+    private double maxPrice;
+    private double minSpread;
+    private double maxSpread;
+    private double minVolume;
+    private double maxVolume;
+    private String feedName;
+    private String venueName;
+    private String symbol;
+    private double publishProbability;
+    private int pricePrecision = 3;
+    private int volumePrecision = 3;
+
+}
